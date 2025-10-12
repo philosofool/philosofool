c21c6a64dfc884a46a08f2b8c9550a68ec7fb414
philosofool <lenhart.lenhart@gmail.com>
2025-10-12 15:43:22 +0000

    Improve Callbacks
    - Move callbacks to separate module
    - Add EarlyStopping
    - Move JSON logger
    - Update history callback and jsoncallback

---
+++ src/philosofool/torch/callbacks.py
@@ -0,0 +1,177 @@
+from __future__ import annotations
+from collections import defaultdict
+from collections.abc import Iterable
+import json
+import os
+import numpy as np
+from typing import TYPE_CHECKING
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision.utils import make_grid
+
+from philosofool.torch.visualize import show_image
+
+if TYPE_CHECKING:
+    from philosofool.torch.nn_loop import GANLoop
+
+
+class EarlyStoppingCallabck:
+    def __init__(self, patience: int, monitor: str = 'test_loss'):
+        self.patience = patience
+        if monitor not in {'val_loss', 'test_loss'}:
+            raise NotImplementedError("EarlyStopping currently only suppots stopping on validation loss.")
+        self.monitor = monitor
+        self._loops_since_improvement = 0
+        self._best = None
+
+    def on_batch_end(self, loop, batch, loss, **kwargs):
+        ...
+
+    def on_epoch_end(self, loop, test_loss, **kwargs):
+        if self.monitor in {'test_loss', 'val_loss'}:
+            return self._determine_improvement(loop, test_loss)
+        raise NotImplementedError("EarlyStopping is only configured to monitor test_loss or val_loss.")
+
+    def _determine_improvement(self, loop, target_metric):
+        if self._best is None:
+            self._best = target_metric
+            return
+        if target_metric < self._best:
+            self._best = target_metric
+            self._loops_since_improvement = 0
+            return
+        self._loops_since_improvement += 1
+        if self._loops_since_improvement >= self.patience:
+            return 'end_fit'
+
+
+class EndOnBatchCallback:
+    """After a number of batches, proceed to the next epoch."""
+
+    def __init__(self, last_batch: int):
+        self.last_batch = last_batch
+
+    def on_batch_end(self, loop, batch: int, **kwargs):
+        if batch == self.last_batch:
+            return 'end_epoch'
+
+
+class SnapshotCallback:
+    """Collect snapshots on an interval.
+
+    With generative models, this collects the outputs on batches and epochs
+    to assess the quality of generated images.
+    """
+
+    def __init__(self, n_images: int, interval: int):
+        self.interval = interval
+        self.n_images = n_images
+        self.snapshots = []
+        self._random_inputs = {}
+
+    def on_batch_end(self, loop: GANLoop, batch, **kwargs):
+        if batch % self.interval != 0:
+            return
+        random_input = self._random_input(loop.generator.input_size).to(loop._device)  # pyright: ignore [reportArgumentType]
+        result = loop.generator(random_input)
+        self.snapshots.append(result)
+
+    def on_epoch_end(self, loop, **kwargs):
+        images = self.snapshots[-1]
+        show_image(make_grid(images.to('cpu'), nrow=4))
+
+    def _random_input(self, size: int) -> torch.Tensor:
+        if size in self._random_inputs:
+            return self._random_inputs[size]
+        random_input = torch.randn((self.n_images, size, 1, 1))
+        self._random_inputs[size] = random_input
+        return random_input
+
+
+class VerboseTrainingCallback:
+    """Provide training metrics to standard ouput during training."""
+
+    def __init__(self, batch_interval: int):
+        self.batch_interval = batch_interval
+
+    def on_epoch_start(self, loop, epoch: int, **kwargs):
+        print(f"Epoch: {epoch}")
+
+    def on_batch_end(self, loop, batch: int, **kwargs):
+        if batch % self.batch_interval != 0:
+            return
+        strings = []
+        for key, value in kwargs.items():
+            strings.append(f"{key}: {value}")
+        print(', '.join(strings))
+
+
+class HistoryCallback:
+    """Create a history of per-epoch results."""
+
+    def __init__(self, batch_end: Iterable[str] = tuple(), epoch_end: Iterable[str] = tuple()):
+        self.batch_end = batch_end
+        self.epoch_end = epoch_end
+        self.history = defaultdict(list)
+        self._batch_history = defaultdict(list) # list-defaultdict is created each epoch start.
+
+    def on_epoch_start(self, loop, **kwargs):
+        self._batch_history = defaultdict(list)
+
+    def on_batch_end(self, loop, batch: int, **kwargs):
+        for key in self.batch_end:
+            value = kwargs.get(key)
+            self._batch_history[key].append(value)
+        return None
+
+    def on_epoch_end(self, loop, **kwargs):
+        for key, values in self._batch_history.items():
+            self.history[key].append(float(np.mean(values)))
+        for key in self.epoch_end:
+            value = kwargs.get(key)
+            self.history[key].append(value)
+
+
+class JSONLoggerCallback:
+    """Save training results to a file."""
+
+    def __init__(self, path):
+        self.path = path
+        try:
+            with open(path, 'r') as f:
+                logs = json.loads(f.read())
+        except FileNotFoundError:
+            self._make_logs_dir(path)
+            logs = {'train_loss': [], 'test_loss': [], 'test_accuracy': []}
+        self.logs = logs
+        self._batch_losses = []
+
+    def _make_logs_dir(self, path: str):
+        directory, filename = os.path.split(path)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+    def on_fit_start(self, loop, train_data: DataLoader, test_data: DataLoader, **kwargs):
+        self._data_size = len(test_data.dataset)    # pyright: ignore [reportArgumentType]
+
+    def on_batch_end(self, loop, **kwargs) -> None:
+        # collect loss on each batch.
+        self._batch_losses.append(kwargs['loss'])
+
+    def on_epoch_end(self, loop, test_loss, **kwargs) -> None:
+        # TODO: implement this on metrics.
+        # self.logs['test_accuracy'].append(correct / self._data_size)
+        self.logs['test_loss'].append(test_loss)
+        self._update_training_loss()
+
+    def on_fit_end(self, loop, **kwargs):
+        with open(self.path, 'w') as f:
+            f.write(json.dumps(self.logs))
+
+    def _update_training_loss(self):
+        # append mean loss from batch_losses
+        if self._batch_losses:
+            mean = np.mean(self._batch_losses)
+            self.logs['train_loss'].append(mean)
+        self._batch_losses = []
---
+++ tests/test_callbacks.py
@@ -0,0 +1,128 @@
+from tempfile import TemporaryDirectory
+import torch
+from torch import nn
+import os
+import json
+from philosofool.torch.callbacks import (
+    EarlyStoppingCallabck, EndOnBatchCallback, HistoryCallback, JSONLoggerCallback, SnapshotCallback, VerboseTrainingCallback
+)
+from philosofool.torch.nn_loop import (
+    GANLoop,
+    Publisher
+)
+from philosofool.torch.nn_models import Discriminator, Generator
+
+
+def test_history_callback():
+    publisher = Publisher()
+    callback = HistoryCallback(batch_end=['loss'], epoch_end=['test_loss'])
+    y_hat, y_hat_val = torch.tensor([.9, .1]), torch.tensor([.9, .1])
+    y_true, y_true_val = torch.tensor([1, 0]), torch.tensor([1, 0])
+    publisher.subscribe('event', callback)
+    publisher.publish('event', 'epoch_start', None)
+    publisher.publish('event', 'batch_end', None, batch=1, loss=.05, y_hat=y_hat, y_true=y_true)
+    publisher.publish('event', 'epoch_end', None, test_loss=.1, y_hat=y_hat_val, y_true=y_true_val)
+    assert callback.history == {'loss': [.05], 'test_loss': [.1]}
+
+
+def test_json_logging_callback(data_loader):
+
+    directory = TemporaryDirectory().name
+    path = os.path.join(directory, 'logs', 'log.json')
+    publisher = Publisher()
+
+    callback = JSONLoggerCallback(path)
+    publisher.subscribe('events', callback)
+    publisher.publish('events', 'fit_start', None, data_loader, data_loader)
+    publisher.publish('events', 'epoch_start', None, epoch=1)
+    publisher.publish('events', 'batch_end', None, loss=.1)
+    publisher.publish('events', 'epoch_end', None, correct=1, test_loss=.2)
+    publisher.publish('events', 'fit_end', None)
+
+    with open(path, 'r') as f:
+        logs = json.loads(f.read())
+    assert callback.logs == logs
+    assert logs['train_loss'] == [.1]
+    assert logs['test_loss'] == [.2]
+    # assert logs['test_accuracy'] == [1 / 2]  # THIS NEEDS IMPLEMENTING
+
+
+def test_end_on_batch():
+    end_on_batch = EndOnBatchCallback(2)
+    assert end_on_batch.on_batch_end(None, batch=2, gen_loss=.1) == 'end_epoch', "Should emit 'end_epoch' signal, gen_loss (unused) is accepeccted."
+    assert end_on_batch.on_batch_end(None, batch=1) is None, "Should not end on batch one. Missing gen_loss is accepted."
+
+
+def test_snapshot_callback():
+    generator = Generator(10, 10)
+    discriminator = Discriminator(10)
+
+    loss = nn.BCEWithLogitsLoss()
+    loop = GANLoop(
+        generator,
+        discriminator,
+        torch.optim.SGD(generator.parameters(), lr=.01, momentum=0),
+        torch.optim.SGD(discriminator.parameters(), lr=.01, momentum=0),
+        loss
+    )
+
+    loop.generator.eval()
+    snapshot_callback = SnapshotCallback(n_images=1, interval=2)
+    snapshot_callback.on_batch_end(loop, batch=2, gen_loss=.1, dis_loss=.1)
+    assert len(snapshot_callback.snapshots ) == 1, "Snapshots should update when called on batch interval."
+    snapshot_callback.on_batch_end(loop, batch=3, gen_loss=.1, dis_loss=.1)
+    assert len(snapshot_callback.snapshots ) == 1, "Snapshots should update only on interval."
+    snapshot_callback.on_batch_end(loop, batch=2, gen_loss=.1, dis_loss=.1)
+    assert len(snapshot_callback.snapshots) == 2, "Snapshots should update when called on batch interval."
+    snapshots = snapshot_callback.snapshots
+    assert torch.allclose(snapshots[0].detach(), snapshots[1].detach())
+
+    snapshot_callback.on_epoch_end(loop, epoch=1)
+    assert snapshot_callback.snapshots[0].shape[0] == snapshot_callback.n_images == 1
+
+
+def test_vebose_training_callback():
+
+    callback = VerboseTrainingCallback(batch_interval=2)
+    callback.on_batch_end(None, batch=1)
+    callback.on_batch_end(None, batch=2, gen_loss=.1, dis_loss=.2)
+    callback.on_epoch_start(None, epoch=12, unused_argument='ignored')
+
+class TestEarlyStopping:
+    def test_emits_end_fit(self):
+        """Assure stops after number of expected turns."""
+        callback = EarlyStoppingCallabck(patience=2, monitor='val_loss')
+        publisher = Publisher()
+        publisher.subscribe('events', callback)
+
+
+        loop = None
+        y = torch.tensor([1, 1])
+        y_hat = torch.tensor([.5, .5])
+        loss = torch.nn.functional.nll_loss(y_hat, y).mean().item()
+
+        for i in range(2):
+            signals = publisher.publish('events', 'epoch_end', loop, test_loss=loss)
+            assert signals == []
+
+        signals = publisher.publish('events', 'epoch_end', loop, test_loss=loss)
+        assert signals == ['end_fit']
+
+    def test_resets_after_improvement(self):
+        callback = EarlyStoppingCallabck(patience=1, monitor='val_loss')
+        publisher = Publisher()
+        publisher.subscribe('events', callback)
+
+
+        loop = None
+        y = torch.tensor([1, 1])
+        y_hat = torch.tensor([.5, .5])
+        loss = torch.nn.functional.nll_loss(y_hat, y).mean().item()
+
+        publisher.publish('events', 'epoch_end', loop, test_loss=loss)
+        signals = publisher.publish('events', 'epoch_end', loop, test_loss=loss - 1)
+        assert signals == []
+
+        publisher.publish('events', 'epoch_end', loop, test_loss=loss)
+        signals = publisher.publish('events', 'epoch_end', loop, test_loss=loss)
+        assert signals == ['end_fit']
